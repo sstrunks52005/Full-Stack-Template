22048eb16b77e9e26dbd0248edcfd0b05a86cce0
Bowie Wan <bowiewan@gmail.com>
2022-04-25 05:05:05 -0500

    added port and mongo connections added more

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
+//can add const PORT = 8000 but dont need to since its in .env file
 
 //3 declare variables before func for global scope - can be used in any function
 let db,
@@ -14,6 +15,12 @@ let db,
     MongoClient.connect(dbConnectionString)         //tells mongo to grab connection string and have credentials to access
         .then(client => {
             console.log(`Connected to database`)     //go to mongo to check your dbName and collection 
-            db = Client.db(dbName)
+            db = client.db(dbName)
             collection = db.collection('startrek-aliens')
         })
+
+//5 add connection port - put in env to hide port
+app.listen(process.env.PORT || PORT, () => {        //if PORT work heroku will assign a port
+    console.log(`Server is running on port`)
+})
+
