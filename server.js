0aa73091961654e99b8b10e5d8089cb0a0a28355
Bowie Wan <bowiewan@gmail.com>
2022-06-22 05:05:05 -0500

    added variables and mongo connection

--- server.js
+++ server.js
@@ -3,3 +3,17 @@ const app = express()
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
+
+//3 declare variables before func for global scope - can be used in any function
+let db,
+    dbConnectionString = process.env.DB_STRING,     //connection string to connect MongoDB. Pull out of environment file
+    dbName = 'startrek-aliens-guide',
+    connection
+
+//4 connect to mongo db
+    MongoClient.connect(dbConnectionString)         //tells mongo to grab connection string and have credentials to access
+        .then(client => {
+            console.log(`Connected to database`)     //go to mongo to check your dbName and collection 
+            db = Client.db(dbName)
+            collection = db.collection('startrek-aliens')
+        })
