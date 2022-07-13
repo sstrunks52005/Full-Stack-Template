26f742584d2df3c618e4ef7602efad80dceafd7c
Bowie Wan <bowiewan@gmail.com>
2022-07-13 01:03:33 -0500

    fixed arrow func error

--- server.js
+++ server.js
@@ -20,11 +20,6 @@ let db,
             collection = db.collection('startrek-aliens')
         })
 
-//5 add connection port - put in env to hide port
-app.listen(process.env.PORT || PORT, () => {        //if PORT work heroku will assign a port
-    console.log(`Server is running on port`)
-})
-
 //6 middleware - need these prior to any crud operations (get, put etc)
 app.set('view engine', 'ejs')               //templating let us run JS in html. shorthand tool
 app.use(express.static('public'))           //setup public folder - lets app automatically serve files in public as they are called upon
@@ -33,10 +28,16 @@ app.use(express.json())                     //JSON - how obj is formatted when s
 app.use(cors())                             //cross origin request, stops cor errors. 
 
 //10 test heroku - listen for url to pass into server and return to different location. express method:
-app.get('/', async(request, respond)) => {
+app.get('/', async(request, respond) => {
     try {
         response.render('index.ejs')  //when visit to home page, render index.ejs to display, if not work send error
     } catch(error) {
         response.status(500).send({message: error.message})
     }
-}
+})
+
+
+//5 add connection port - put in env to hide port
+app.listen(process.env.PORT || PORT, () => {        //if PORT work heroku will assign a port
+    console.log(`Server is running on port`)
+})
